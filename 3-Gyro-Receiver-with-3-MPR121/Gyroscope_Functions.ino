2adfc992ae4dab231718ae0e3c428a6eda60cb7e
Adam Cole <acole9@gmail.com>
2022-05-24 17:49:08 +0100

    working gyro 3

--- 3-Gyro-Receiver-with-3-MPR121/Gyroscope_Functions.ino
+++ 3-Gyro-Receiver-with-3-MPR121/Gyroscope_Functions.ino
@@ -1,4 +1,8 @@
 float GYRO_RANGE = 90.0;
+float SMOOTH_VALUE_FACTOR = 0.2;
+
+String inDataString;
+
 
 // UTILITIES
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
@@ -8,6 +12,14 @@ float mapfloat(float x, float in_min, float in_max, float out_min, float out_max
 float mapGyro(float val) {
   return mapfloat(val, -GYRO_RANGE, GYRO_RANGE, -1.0, 1.0);
 }
+
+float getSmoothedValue(float value, float target) {
+  return value + (target - value) * SMOOTH_VALUE_FACTOR;
+}
+//gy1yTarget=event.orientation.y;
+// gy1zTarget=event.orientation.z;
+// gy1y=gy1y+(gy1Target-gy1y)*0.2;
+// gy2y=gy2y+(gy2Target-gy2y)*0.2;
 
 void initializeGyros(){
   if(!bno.begin())
@@ -37,51 +49,58 @@ void initializeGyros(){
 void readGyro1Data() {
  sensors_event_t event; 
  bno.getEvent(&event);
- gy1y=mapGyro(event.orientation.y);
- gy1z=mapGyro(event.orientation.z);
-  }
+
+ float mappedEventY = mapGyro(event.orientation.y);
+ float mappedEventZ = mapGyro(event.orientation.z);
+
+ gy1y = getSmoothedValue(gy1y, mappedEventY);
+ gy1z = getSmoothedValue(gy1z, mappedEventZ);
+}
 
 void readGyro2Data(){
-   sensors_event_t event2; 
+ sensors_event_t event2; 
  bno2.getEvent(&event2);
- gy2y=mapGyro(event2.orientation.y);
- gy2z=mapGyro(event2.orientation.z);
-  }
+ 
+ float mappedEventY = mapGyro(event2.orientation.y);
+ float mappedEventZ = mapGyro(event2.orientation.z);
+
+ gy2y = getSmoothedValue(gy2y, mappedEventY);
+ gy2z = getSmoothedValue(gy2z, mappedEventZ);
+}
 
 void readGyro3Data() {
-  if (Serial1.available()) {
-      //char inByte = Serial1.read();
-//    Serial.write(inByte);
-     if (Serial1.read() == 'b') {
-       for (dataLength = 0; dataLength < 10; dataLength++) {
-        delay(2);
-        inData[dataLength] = Serial1.read();
-         if (inData[dataLength] == 'n') {
-           inData[dataLength] = ',';
-           break;
-         }
+  String readInValues = "a";
+  String outputString = "";
+
+   while (Serial1.available()) {
+        char recieved = Serial1.read();
+        inDataString += recieved; 
+
+        // Process message when new line character is recieved
+        if (recieved == '\n')
+        {
+//            Serial.print("Arduino Received: ");
+//            Serial.print(inDataString);
+            outputString = inDataString;
+            inDataString = ""; // Clear recieved buffer
         }
-      }
     }
 
-  gyro3Readings="";
-  for (int i = 0; i < 10; i++) {
-    gyro3Readings = gyro3Readings + inData[i];
-  }
-  
-  //Serial.println(gyro3Readings);
-    getGyro3Values();
+    String cleanDataString = outputString.substring(1, outputString.length() - 3);
+    gyro3Readings = cleanDataString;
 
+    getGyro3Values();
 }
 
 
 void getGyro3Values(){
    int firstCommaIndex = gyro3Readings.indexOf(',');
-   int secondCommaIndex = gyro3Readings.indexOf(',', firstCommaIndex+1);
-
    String firstValue = gyro3Readings.substring(0,firstCommaIndex);
-   String secondValue = gyro3Readings.substring(firstCommaIndex+1,secondCommaIndex);
+   String secondValue = gyro3Readings.substring(firstCommaIndex+1);
+    
+   float mappedEventY = mapGyro(firstValue.toFloat());
+   float mappedEventZ = mapGyro(secondValue.toFloat());
 
-   gy3z= mapGyro(firstValue.toInt());
-   gy3y= mapGyro(secondValue.toInt());
+   gy3y = getSmoothedValue(gy3y, mappedEventY);
+   gy3z = getSmoothedValue(gy3z, mappedEventZ);
  }
